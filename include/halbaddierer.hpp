42fc0bb3f91bd24518af914080ad3c3194a73ba9
Maximilian <max.stocker@gmx.de>
2022-04-27 19:59:13 +0200

    intern und update funktionen hinzugefügt

--- include/halbaddierer.hpp
+++ include/halbaddierer.hpp
@@ -12,8 +12,22 @@ private:
     /* data */
 public:
     Halbaddierer(Schnittstelle* e0, Schnittstelle* e1, Schnittstelle* a0, Schnittstelle* a1);
+    void update();
 };
 
 Halbaddierer::Halbaddierer(Schnittstelle* e0, Schnittstelle* e1, Schnittstelle* a0, Schnittstelle* a1)
 {
+    intern.push_back(e0);
+    intern.push_back(e1);
+    intern.push_back(a0);
+    intern.push_back(a1);
+
+    Xor2 xor2(e0, e1, a0);
+    Und2 und2(e0, e1, a1);
+
+    xor2.update();
+    und2.update();
+}
+
+void Halbaddierer::update(){
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main()
 {
-    std::cout << "test" << std::endl;
+    std::cout << "Test" << std::endl;
     return 0;
 }
